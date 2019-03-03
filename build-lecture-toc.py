763a8905d3235f0f4d910fe69b3ce0db04a6075d
Kolowitz <bkolowitz@gmail.com>
2019-03-03 14:40:52 -0500

    update toc

--- build-lecture-toc.py
+++ build-lecture-toc.py
@@ -4,9 +4,10 @@ if __name__ == "__main__":
     os.chdir('lectures')
     
     with open('lectures.md', 'w') as f:
-        f.write('# Lectures\n')
+        f.write('# Lectures\n\n')
         for file in glob.glob("*.md"):
             if file == 'lectures.md':
                 continue
+            print(file)
             f.writelines(f'* [{file}]({file})\n')
 
--- lectures/lectures.md
+++ lectures/lectures.md
@@ -1,3 +1,4 @@
 # Lectures
+
 * [loading-synthea-data.md](loading-synthea-data.md)
 * [files.md](files.md)
